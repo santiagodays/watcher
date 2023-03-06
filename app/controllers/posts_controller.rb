e5375e56e7da11c53b757a49f60ad76c1bfcf76d
Santiago Diaz Di Meo <santiagodiazdimeo@Santiagos-MacBook-Air.local>
2023-03-05 21:45:53 -0600

    Image uploading and post editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, only: %i[new edit create update destroy ]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -36,6 +36,11 @@ class PostsController < ApplicationController
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
+    if @post.user_id != current_user.id
+      redirect_to post_url(@post), alert: "You don't have permission to edit this post."
+      return
+    end
+
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
@@ -46,6 +51,7 @@ class PostsController < ApplicationController
       end
     end
   end
+
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
@@ -66,7 +72,7 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :content, :category, :user_id, :image_slug).tap do |whitelisted|
-        whitelisted[:image] = params[:post][:image_slug] if params[:post][:image_slug].present?
+        whitelisted[:image_slug] = params[:post][:image_slug] if params[:post][:image_slug].present?
       end
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   mount_uploader :image_slug, ContentUploader
+
+  def image_url(image_slug)
+    if File.exist?("public/uploads/post/image_slug/#{self.id}/#{image_slug}")
+      return "/uploads/post/image_slug/#{self.id}/#{image_slug}"
+    else
+      return ""
+    end
+  end
 end
